41d204919178a0747918c5c468dbb9823ac6dc1a
Daniel Sobral <danielsobralnascimento@hotmail.com>
2025-10-28 19:18:41 +0000

    add sql injection protection for all queries

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,9 +3,10 @@ import database from "infra/database";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseMaxConnectionsResult = await database.query(
-    "SELECT setting::int FROM pg_settings WHERE name='max_connections';",
-  );
+  const databaseMaxConnectionsResult = await database.query({
+    text: "SELECT setting::int FROM pg_settings WHERE name=$1;",
+    values: ["max_connections"],
+  });
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].setting;
 
@@ -17,11 +18,14 @@ async function status(request, response) {
   const databaseActiveConnectionsValue =
     databaseActiveConnectionsResult.rows[0].count;
 
-  const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = parseFloat(
-    databaseVersionResult.rows[0].server_version,
-  );
+  const databaseVersionResult = await database.query({
+    text: "SELECT setting::float FROM pg_settings WHERE name = $1;",
+    values: ["server_version"],
+  });
 
+  const databaseVersionValue = parseFloat(
+    databaseVersionResult.rows[0].setting,
+  );
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
