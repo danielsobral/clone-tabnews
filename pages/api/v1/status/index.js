cfd380a486b9345b933cda5a780db039057d7d4e
Daniel Sobral <danielsobralnascimento@hotmail.com>
2025-10-28 18:58:28 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,36 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1 as sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ mensagem: "Alunos do curso.dev são pessoas acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SELECT setting::int FROM pg_settings WHERE name='max_connections';",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].setting;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseActiveConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseActiveConnectionsValue =
+    databaseActiveConnectionsResult.rows[0].count;
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = parseFloat(
+    databaseVersionResult.rows[0].server_version,
+  );
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: databaseMaxConnectionsValue,
+        active_connections: databaseActiveConnectionsValue,
+        version: databaseVersionValue,
+      },
+    },
+  });
 }
 export default status;
